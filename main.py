ccbdffc791fbe0a0c40640c7c1845d93ad897ca8
shaypheraddison <149912769+shaypheraddison@users.noreply.github.com>
2025-07-21 14:48:07 -0500

    added rotating of player ship in-game

--- main.py
+++ main.py
@@ -18,10 +18,12 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         screen.fill("black")
         player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
+        player.update(dt)
 
 
     
