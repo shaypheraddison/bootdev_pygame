5f20e985501dd66f0008d043ec2489575152d804
shaypheraddison <149912769+shaypheraddison@users.noreply.github.com>
2025-07-21 14:32:02 -0500

    added the player model drawn on ch.2 lesson 5

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import *
 
 
 def main():
@@ -8,14 +9,22 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
-        pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        
+        player.draw(screen)
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
+
+    
         
         
 
